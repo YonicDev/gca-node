680e82aafcfbc974cc33e7647d5432dbf83b59ed
yonic <yonicstudios@gmail.com>
2017-02-05 15:37:58 +0100

    Forgot to add nw_configure.sh

---
+++ nw_configure.sh
@@ -0,0 +1,7 @@
+export npm_config_target=0.20.1
+export npm_config_arch=x64
+export npm_config_target_arch=x64
+export npm_config_runtime=node-webkit
+export npm_config_build_from_source=true
+export npm_config_node_gyp=$(which nw-gyp)
+npm install
